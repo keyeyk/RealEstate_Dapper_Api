09832855ec93562de625164134575676cb81a53f
Kemal Alper <kmlkckk0@gmail.com>
2025-01-06 16:57:29 +0300

    Add project files.

---
+++ RealEstate_Dapper_Ui/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_Ui.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RealEstate_Dapper_Ui/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RealEstate_Dapper_Ui.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
